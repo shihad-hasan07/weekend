1fae3ac4bb32028607faf4a9a21d2d3dfca171ce
shihad-hasan07 <shihadhasan607255@gmail.com>
2025-10-23 19:53:11 +0600

    Banner section 90% complete

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className='container mx-auto px-20 mt-20 flex justify-between'>
 
             {/* left side */}
-            <div>
+            <div className="relative">
                 <p className='text-7xl font-bold leading-19 '>Up Your <span className='text-[#20b486]'>Skills</span>
                     <br />To <span className='text-[#20b486]'>Advance</span> Your
                     <br /> <span className='text-[#20b486]'>Career</span>Path
@@ -15,11 +15,33 @@ const Banner = () => {
                     <button className='bg-[#20B486] px-7 text-lg py-3 rounded-lg font-semibold text-white'>Get Started</button>
                     <button className="ml-5 bg-[#eafff9] px-7 text-lg py-3 rounded-lg font-semibold text-[#20b486]">Get free trial</button>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+
+
+                <div className="mt-12 flex gap-10">
+                    <div className="flex items-center gap-2">
+                        <img src="/banner/speakilng.png" alt="" />
+                        <p className="text-[17px] text-gray-500 font-semibold">Public Speaking</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <img src="/banner/Briefcase.png" alt="" className="" />
+                        <p className="text-[17px] text-gray-500 font-semibold">Career-Oriented</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <img src="/banner/idea.png" alt="" />
+                        <p className="text-[17px] text-gray-500 font-semibold">Creative Thinking</p>
+                    </div>
+                </div>
+
+
+                <div className="absolute -top-8 -left-30 -z-50">
+                    <div className="relative w-[400] h-[400] border rounded-full">
+                        <div className="absolute w-[12] h-[12] rounded-full top-8 left-19 bg-green-500"></div>
+                        <div className="absolute top-[40] left-[40] w-[320] h-[320] border rounded-full">
+                            <div className="absolute top-[40] left-[40] w-[240] h-[240] border rounded-full">
+                                <div className="absolute top-[40] left-[40] w-[160] h-[160] border rounded-full"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -61,6 +83,18 @@ const Banner = () => {
 
                     {/* a small circle  */}
                     <div className="absolute bottom-10 w-[40] h-[40] rounded-full bg-[#20b486]"></div>
+
+
+                    <div className="absolute -bottom-40 -right-35 -z-50">
+                        <div className="relative w-[400] h-[400] border rounded-full">
+                            <div className="absolute w-[14] h-[14] rounded-full bottom-20 left-[27] bg-green-500"></div>
+                            <div className="absolute top-[40] left-[40] w-[320] h-[320] border rounded-full">
+                                <div className="absolute top-[40] left-[40] w-[240] h-[240] border rounded-full">
+                                    <div className="absolute top-[40] left-[40] w-[160] h-[160] border rounded-full"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
