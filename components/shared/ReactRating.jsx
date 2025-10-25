de93e2085cf48ece71742b163ce6edfcaea7372d
shihad-hasan07 <shihadhasan607255@gmail.com>
2025-10-25 13:17:38 +0600

    trying to fix the error

--- components/shared/ReactRating.jsx
+++ components/shared/ReactRating.jsx
@@ -1,43 +1,11 @@
-// 'use client'
-// import { MdOutlineStarOutline, MdOutlineStarPurple500 } from "react-icons/md";
-
-
-
-
-// const ReactRating = ({ rating }) => {
-   
-
-//     return (
-//         <>
-           
-//         </>
-//     )
-// };
-
-// export default ReactRating;
-
-
-
-
-'use client';
 import React from 'react';
-import ReactStars from 'react-rating-stars-component';
-import { MdOutlineStarOutline, MdOutlineStarPurple500 } from "react-icons/md";
 
-const ReactRating = ({ rating }) => {
-  return (
-    <ReactStars
-      count={5}                 
-      value={5}         
-      edit={false}               
-      isHalf={true}              
-      size={21}                  
-      activeColor="#ff9b26"    
-      color="#e0e0e0"         
-      emptyIcon={<MdOutlineStarOutline />}
-      filledIcon={<MdOutlineStarPurple500 />}
-    />
-  );
+const ReactRating = ({rating}) => {
+    return (
+        <div>
+sfsf  {rating}
+        </div>
+    );
 };
 
 export default ReactRating;
